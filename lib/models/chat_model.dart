78b8c2e5d12cb7bb30220eb51c6323490aadc7cb
Qwequ_Sandies <sandiesoduro@gmail.com>
2020-12-03 20:19:04 +0000

    update status.dart

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -12,23 +12,21 @@ List<ChatModel> dummyChat = [
       name: "Shelby",
       message: "Chale you get tuts for dart",
       time: "11:30",
-      avatarUrl:
-          "https://i.pinimg.com/originals/c7/96/38/c79638865c40c5652185e2d7450b614a.jpg"),
+      avatarUrl: 'https://picsum.photos/250?image=9'),
   new ChatModel(
       name: "Akrofi ",
       message: "COD anaa!",
       time: "3:30",
-      avatarUrl:
-          "https://i.pinimg.com/originals/c7/96/38/c79638865c40c5652185e2d7450b614a.jpg"),
+      avatarUrl: "https://picsum.photos/250?image=9"),
   new ChatModel(
       name: "Tera",
-      message: "Wassup, you dey house !",
+      message: "Wassup, you dey house ",
       time: "5:00",
       avatarUrl:
           "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
   new ChatModel(
       name: "Enam",
-      message: "I'm good!",
+      message: "I'm good",
       time: "10:30",
       avatarUrl:
           "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
--- lib/screens/status.dart
+++ lib/screens/status.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
 
-class StatusScreen extends StatelessWidget {
+class StatusScreen extends StatefulWidget {
+  @override
+  _StatusScreenState createState() => _StatusScreenState();
+}
+
+class _StatusScreenState extends State<StatusScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Status',
-        style: TextStyle(fontSize: 20),
+    return Container(
+      color: Colors.grey[300],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: ListTile(
+                leading: Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage('images/me.jpg'),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 1,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.green, shape: BoxShape.circle),
+                      ),
+                    )
+                  ],
+                ),
+                title: Text(
+                  'My Status',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  'Tap to add status update',
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: Text(
+              "Recent updates",
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            padding: const EdgeInsets.all(5),
+            color: Colors.white,
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('images/102.jpg'),
+                  ),
+                  title: Text(
+                    'Tera',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    '3 hours ago',
+                  ),
+                  onTap: () {},
+                )
+              ],
+            ),
+          ))
+        ],
       ),
     );
   }
