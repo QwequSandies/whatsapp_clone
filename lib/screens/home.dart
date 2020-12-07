cc0b0bde7bec0c1f4e902b8c0fa1db7aa1b02742
Qwequ_Sandies <sandiesoduro@gmail.com>
2020-12-07 00:22:55 +0000

    modify floating button visibility

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,58 +16,62 @@ class _HomePageState extends State<HomePage>
   @override
   void initState() {
     _tabController = new TabController(length: 4, vsync: this, initialIndex: 1);
-    // _tabController.addListener(() {
-    //   if (_tabController.index == 1) {
-    //     setState(() {
-    //       var showFloatingB = true;
-    //     });
-    //   } else {
-    //     setState(() {
-    //       var showFloatingB = false;
-    //     });
-    //   }
-    // });
+    _tabController.addListener(() {
+      if (_tabController.index == 1) {
+        setState(() {
+          // ignore: unused_local_variable
+          var showFloatingButton = true;
+        });
+      } else {
+        setState(() {
+          // ignore: unused_local_variable
+          var showFloatingButton = false;
+        });
+      }
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('WhatsApp'),
-          elevation: 0.5,
-          bottom: TabBar(
-            controller: _tabController,
-            indicatorColor: Colors.white,
-            tabs: [
-              Tab(icon: Icon(Icons.camera_alt)),
-              Tab(text: 'CHATS'),
-              Tab(text: 'STATUS'),
-              Tab(text: 'CALLS'),
-            ],
-          ),
-          actions: [
-            Icon(Icons.search),
-            Padding(padding: const EdgeInsets.symmetric(horizontal: 5)),
-            Icon(Icons.more_vert),
-          ],
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            CameraScreen(),
-            ChatScreen(),
-            StatusScreen(),
-            CallsScreen(),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
+    bool showFloatingButton = true;
+        return Scaffold(
+            appBar: AppBar(
+              title: Text('WhatsApp'),
+              elevation: 0.5,
+              bottom: TabBar(
+                controller: _tabController,
+                indicatorColor: Colors.white,
+                tabs: [
+                  Tab(icon: Icon(Icons.camera_alt)),
+                  Tab(text: 'CHATS'),
+                  Tab(text: 'STATUS'),
+                  Tab(text: 'CALLS'),
+                ],
+              ),
+              actions: [
+                Icon(Icons.search),
+                Padding(padding: const EdgeInsets.symmetric(horizontal: 5)),
+                Icon(Icons.more_vert),
+              ],
+            ),
+            body: TabBarView(
+              controller: _tabController,
+              children: [
+                CameraScreen(),
+                ChatScreen(),
+                StatusScreen(),
+                CallsScreen(),
+              ],
+            ),
+            floatingActionButton: showFloatingButton ? FloatingActionButton(
           backgroundColor: Theme.of(context).primaryColor,
           child: Icon(
             Icons.message,
             color: Colors.white,
           ),
           onPressed: () => print('open chats'),
-        ));
+        ): null
+        );
   }
 }
